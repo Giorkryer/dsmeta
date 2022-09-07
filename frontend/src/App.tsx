f110fd5f540ee78cdb90a89e4db5ad9e92c2ab58
Giorkryer <giokryer@gmail.com>
2022-09-07 09:17:17 -0300

    botao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá mundo</h1>
+
+    <NotificationButton />
+    
+    </>
   )
 }
 
---
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from './iconNotification.svg'
+
+import './style.css'
+
+function NotificationButton(){
+    return(
+        <div className="dsmeta-red-btn"> 
+        <img src={icon} alt="" /></div>
+    )
+}
+
+export default NotificationButton
